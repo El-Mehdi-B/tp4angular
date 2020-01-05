aade8c6b1c8ee2e7d832e37904ea7467b7fd5608
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2020-01-05 17:53:45 +0100

    question 3.6

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,12 @@ import {trigger, style, transition, animate, keyframes, query, stagger} from '@a
   animations : [ trigger('animobjectifs' ,[
     transition ( '*=>*' ,[
     query(':enter', style({opacity: 0}), {optional: true}),
-    query(':enter', stagger('300ms',[ animate ( '.6s ease-in' , keyframes ([
-    style({opacity: 0, transform: 'translateY(-75%)', offset: 0}), style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+    query(':enter', stagger('300ms',[ animate ( '0.7s ease-in' , keyframes ([
+    style({opacity: 0, transform: 'translateY(-205%)', offset: 0}), style({opacity: .5, transform: 'translateY(85px)', offset: .3}),
     style({opacity: 1, transform: 'translateY(0)', offset: 1}), ]))]),{optional: true})
     ]) ])]
 
-    
+
 })
 
 export class HomeComponent implements OnInit {
